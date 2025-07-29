fce2e3b2b5d2051f8217dbc3a4eeb26547317969
Andr3Ferreira <bored@sambista.localdomain>
2025-07-29 14:59:49 -0400

    Playing around with styles

--- deployNInstall.sh
+++ deployNInstall.sh
@@ -3,7 +3,7 @@
 # ./gradlew clean
 
 # Run ./gradlew assembleDebug
-# ./gradlew assembleDebug
+./gradlew assembleDebug
 
 # If successful, install app on phone
 adb install -r app/build/outputs/apk/debug/app-debug.apk
