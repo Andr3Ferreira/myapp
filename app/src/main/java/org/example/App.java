7aa0cf18cab73d45e580c2c96cea8d98d3c93715
Andr3Ferreira <bored@sambista.localdomain>
2025-08-04 07:44:46 -0400

    Basic Layout for Login Activity

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,9 +1,12 @@
 package org.example;
 
 import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class App extends AppCompatActivity {
 
@@ -11,12 +14,17 @@ public class App extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        TextView tv = new TextView(this);
-        // tv.setText("Hello World");
-        // setContentView(tv);
-
+        // TextView tv = new TextView(this);
+        RelativeLayout loginBox = new RelativeLayout(this);
         Button btn = new Button(this);
+
         setContentView(R.layout.app_main);
+
+        // Hides Title Bar at the top of activity. Must be placed in every activity.
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
     }
 
     // public String getGreeting() {
