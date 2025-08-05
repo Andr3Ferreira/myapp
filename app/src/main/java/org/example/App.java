e43de92bfd0c7be7c04da2a7751d13ee208e4b7e
Andr3Ferreira <bored@sambista.localdomain>
2025-08-05 07:32:18 -0400

    Adding buttons and links to main activity

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,12 +1,20 @@
 package org.example;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 //import org.example.databinding.ActivityMainBinding;
 
 public class App extends AppCompatActivity {
@@ -28,6 +36,31 @@ public class App extends AppCompatActivity {
             getSupportActionBar().hide();
         }
 
+        TextView myTextView = findViewById(R.id.signup_view);
+        String fullText = "Don't have an account yet? Sign up.";
+        SpannableString spannableString = new SpannableString(fullText);
+
+        // Define the start and end indices of the clickable part
+        int startIndex = fullText.indexOf("Sign up");
+        int endIndex = startIndex + "Sign up".length();
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                // Handle the click event here
+                Toast.makeText(widget.getContext(), "Link clicked!", Toast.LENGTH_SHORT).show();
+                // You can open a URL, navigate to another activity, etc.
+            }
+            
+        };
+
+        spannableString.setSpan(clickableSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        myTextView.setText(spannableString);
+        // Important: Enable link movement for the TextView
+        myTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        
+        
 //        binding = ActivityMainBinding.inflate(getLayoutInflater());
 //        View view = binding.getRoot();
 //        setContentView(view);
